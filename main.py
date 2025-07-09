ec151b0b3921deadd59992d1e8594fd65b0f6220
Darion Welch <darion.welch@gmail.com>
2025-07-09 12:54:48 -0500

    add sleep between record and playback

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import recording_manager
 import playback_manager
 import threading
+import time
 
 def main():
     rm = recording_manager.RecordingManager()
@@ -8,7 +9,7 @@ def main():
 
     threading.Timer(5, rm.stop_recording).start()
     filepath = rm.start_recording()
-    
+    time.sleep(0.5)
     pm.play_file(filepath)
 
 if __name__ == "__main__":
