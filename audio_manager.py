ba3a8bc468dc17091644c5cccc5080d2c32472d1
Darion Welch <darion.welch@gmail.com>
2025-07-08 19:17:18 -0500

    another try

--- audio_manager.py
+++ audio_manager.py
@@ -27,14 +27,12 @@ class AudioManager:
 
         # Configure the wave file
         file = wave.open(filepath, 'wb')
-        file.setnchannels(2)
+        file.setnchannels(2)  # Stereo recording
         file.setsampwidth(2) # PCM_FORMAT_S16_LE
         file.setframerate(44100)
 
         # Open the device in non-blocking capture mode
-        inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NONBLOCK,
-                channels=1, rate=44100, format=alsaaudio.PCM_FORMAT_S16_LE,
-                periodsize=160)
+        inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NONBLOCK, channels=2, rate=44100, format=alsaaudio.PCM_FORMAT_S16_LE, periodsize=160, device='default')
 
         inp.setperiodsize(160)
 
